73f6da564f6277ea85fac8e9bec5422a246c34e3
Alyssa Nah Xiao Ting <54824858+alyssanxt@users.noreply.github.com>
2023-11-29 14:34:28 +0800

    refactor hooks into useGame hooks

--- src/modules/GameWithHooks.tsx/GameWithHooks.tsx
+++ src/modules/GameWithHooks.tsx/GameWithHooks.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC, useCallback, useState, useMemo } from 'react';
 
 import { GameLevels, LevelNames, GameSettings } from '@/modules/GameSettings';
 
@@ -9,28 +9,72 @@ import { CellState, Coords, Field, fieldGenerator, generateFieldWithDefaultState
 import { Wrapper } from '@/components/Game/Wrapper';
 import { GameArea } from '@/components/Game/GameArea';
 import { openCell } from '@/helper/CellsManipulator';
+import { GameOver } from '@/components/Game/GameOver';
 
-// import { useGame } from './useGame';
+import { useGame } from './useGame';
 
 export const GameWithHooks: FC = () => {
-    const [level, setLevel] = useState<LevelNames>('beginner')
+    // const [level, setLevel] = useState<LevelNames>('beginner')
+    // const [isGameOver, setIsGameOver] = useState(false)
+    // const [isWin, setIsWin] = useState(false)
+    // const [size, bombs] = GameSettings[level]
+    // //hiden
+    // const [playerField, setPlayerField] = useState<Field>(
+    //     generateFieldWithDefaultState(size, CellState.hidden)
+    // )
 
-    const [size, bombs] = GameSettings[level]
-    const [playerField, setPlayerField] = useState<Field>(
-        generateFieldWithDefaultState(size, CellState.hidden)
-    )
-    const gameField = fieldGenerator(size, bombs/ (size * size));
 
-    const onClick = (coords: Coords) => {
-        const newPlayerfield =  openCell(coords,playerField,gameField);
-        setPlayerField([...newPlayerfield]);
-    };
-    const onChangeLevel = ({target: {value: level}}: React.ChangeEvent<HTMLSelectElement>) => {
-        setLevel(level as LevelNames);
-        const [size] = GameSettings[level as LevelNames]
-        const newPlayerfield =  generateFieldWithDefaultState(size,CellState.hidden);
-        setPlayerField([...newPlayerfield]);
-    };
+    // const [gameField,setGameField] = useState<Field>(
+    //     fieldGenerator(size, bombs/ (size * size))
+    // );
+
+    // const onClick = (coords: Coords) => {
+    //     try {
+    //         const newPlayerfield =  openCell(coords,playerField,gameField);
+    //         setPlayerField([...newPlayerfield]);
+    //     } catch (e) {
+    //         setIsGameOver(true)
+    //         setPlayerField([...gameField])
+    //     }
+        
+    // };
+    // const onChangeLevel = ({target: {value: level}}: React.ChangeEvent<HTMLSelectElement>) => {
+    //     const newSettings = GameSettings[level as LevelNames]
+    //     resetHandler(newSettings)
+    // };
+    // const resetHandler = ([size, bombs]: [number, number]) => {
+    //     // fixed game field
+    //     const newGameField = fieldGenerator(size, bombs / (size * size));
+    //     const newPlayerfield =  generateFieldWithDefaultState(size,CellState.hidden);
+
+    //     // updates with the user's moves
+    //     setPlayerField([...newPlayerfield]);
+    //     setGameField([...newGameField]);
+    //     setIsGameOver(false)
+    //     setIsWin(false)
+    // };
+
+    // const onReset = () => resetHandler([size, bombs]);
+    const {
+        level,
+        isGameOver,
+        isWin,
+        settings,
+        playerField,
+        onClick,
+        onChangeLevel,
+        onReset,
+    } = useGame()
+
+    const[, bombs] = settings;
+    // const onChangeLevelHandler = useCallback(
+    //     ({ target: { value: level } }: React.ChangeEvent<HTMLSelectElement>) => {
+    //       setSearchParams({ level });
+    //       onChangeLevel(level as LevelNames);
+    //     },
+    //     // Stryker disable next-line ArrayDeclaration
+    //     []
+    //   );
   return (
     <Wrapper>    
         <Top feature="Flag" firstAction="ctrl" secondAction='click'>
@@ -41,16 +85,14 @@ export const GameWithHooks: FC = () => {
             time = "00"
             levels={GameLevels as unknown as string[]}
             defaultLevel={level}
-            bombs="10"
-            onReset={() => null}
-            // onChangeLevel={({target: {value }}) => 
-            //     // console.log("this is value", value)}
-            //     // onChangeLevel= { () => 
-            //     // console.log("this is value!!")}
-            //     setLevel(value as LevelNames)}
-
-            onChangeLevel={onChangeLevel}
+            bombs={String(bombs)}
+            onReset={onReset}
+            onChangeLevel={({ target: { value: level } 
+            }: React.ChangeEvent<HTMLSelectElement>) => {
+                onChangeLevel(level as LevelNames);
+            }}
         ></Scoreboard>
+        {isGameOver && <GameOver onClick={onReset} isWin = {isWin}></GameOver>}
         <Grid
             onClick = {onClick}
             onContextMenu= {() => null}>
---
+++ src/modules/GameWithHooks.tsx/useGame.ts
@@ -0,0 +1,89 @@
+import { useState, useCallback } from 'react';
+
+import {
+  Field,
+  CellState,
+  generateFieldWithDefaultState,
+  fieldGenerator,
+  Coords,
+} from '@/helper/Field'
+import { openCell } from '@/helper/CellsManipulator';
+// import { setFlag } from '@/helper/setFlag';
+
+import { LevelNames, GameSettings } from '@/modules/GameSettings';
+
+// import { useStatus } from './useStatus';
+// import { useSettings } from './useSettings';
+// import { useTime } from './useTime';
+
+interface ReturnType {
+  level: LevelNames;
+//   time: number;
+  isGameOver: boolean;
+//   isGameStarted: boolean;
+  isWin: boolean;
+  settings: [number, number];
+  playerField: Field;
+//   gameField: Field;
+//   flagCounter: number;
+  onClick: (coords: Coords) => void;
+//   onContextMenu: (coords: Coords) => void;
+  onChangeLevel: (level: LevelNames) => void;
+  onReset: () => void;
+}
+
+export const useGame = (): ReturnType =>{
+    const [level, setLevel] = useState<LevelNames>('beginner')
+    const [isGameOver, setIsGameOver] = useState(false)
+    const [isWin, setIsWin] = useState(false)
+    const [size, bombs] = GameSettings[level]
+    //hiden
+    const [playerField, setPlayerField] = useState<Field>(
+        generateFieldWithDefaultState(size, CellState.hidden)
+    )
+
+
+    const [gameField,setGameField] = useState<Field>(
+        fieldGenerator(size, bombs/ (size * size))
+    );
+
+    const onClick = (coords: Coords) => {
+        try {
+            const newPlayerfield =  openCell(coords,playerField,gameField);
+            setPlayerField([...newPlayerfield]);
+        } catch (e) {
+            setIsGameOver(true)
+            setPlayerField([...gameField])
+        }
+        
+    };
+    const onChangeLevel = (level: LevelNames) => {
+        setLevel(level as LevelNames)
+        const newSettings = GameSettings[level as LevelNames]
+        resetHandler(newSettings)
+    };
+    const resetHandler = ([size, bombs]: [number, number]) => {
+        // fixed game field
+        const newGameField = fieldGenerator(size, bombs / (size * size));
+        const newPlayerfield =  generateFieldWithDefaultState(size,CellState.hidden);
+
+        // updates with the user's moves
+        setPlayerField([...newPlayerfield]);
+        setGameField([...newGameField]);
+        setIsGameOver(false)
+        setIsWin(false)
+    };
+
+    const onReset = () => resetHandler([size, bombs]);
+
+    return {
+        level,
+        isGameOver,
+        isWin,
+        settings: [size, bombs],
+        playerField,
+        onClick,
+        onChangeLevel,
+        onReset,
+    };
+};
